8c726936b986a988f08ad44763d416f065b4d3af
Yanqc <906763115@qq.com>
2020-05-11 14:56:03 +0800

    多页面项目初始化配置

--- src/views/test/App.vue
+++ src/views/test/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="test">
+    <router-link></router-link>
+    <div>
+        <a href="index.html">跳转新的页面</a>
+      </div>
   </div>
 </template>
+
 
 <style>
 #app {
